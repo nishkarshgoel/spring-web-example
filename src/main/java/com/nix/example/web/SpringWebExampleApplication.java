eb2eefb1c1663d2174f9df99df41e10bd54de614
nishkarshgoel <goel.nix@gmail.com>
2021-06-17 12:44:14 +0530

    Added initial project structure

---
+++ src/main/java/com/nix/example/web/SpringWebExampleApplication.java
@@ -0,0 +1,13 @@
+package com.nix.example.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nix/example/web/SpringWebExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nix.example.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
